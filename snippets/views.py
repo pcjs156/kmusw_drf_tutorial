d53b5c9179d415e4bfe38a49de5ccb68c73db119
Yubin Park <itqkrdbqls1001@naver.com>
2021-08-10 15:18:33 +0900

    feat(snippets): Snippet 모델 CRUD View 추가

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
+
+@csrf_exempt
+def snippet_list(request):
+    """
+    모든 code snippet의 목록을 반환하거나, 새 snippet을 생성함
+    """
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    """
+    code snippet의 조회, 수정, 삭제를 수행함
+    """
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        else:
+            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
