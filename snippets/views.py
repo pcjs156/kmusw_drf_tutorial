1cb12810a0326ef918973c0d606564e0852d876e
Yubin Park <itqkrdbqls1001@naver.com>
2021-08-16 16:26:10 +0900

    feat(snippets): SnippetDetail view에 Custom Permission 추가
    Snippet 사용자만 쓰기 권한을 가지도록 수정

--- snippets/views.py
+++ snippets/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from rest_framework import permissions
 
 from snippets.models import Snippet
+from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
 
 
@@ -26,7 +27,8 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
 
 
 class UserList(generics.ListAPIView):
